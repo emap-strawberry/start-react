7d194b87a99e81fe9fba565e30a62ea8c05cab39
emap-strawberry <melon.pan.san@gmail.com>
2020-05-13 10:05:09 +0900

    new commit
    the first commit for start-react

---
+++ docs/precache-manifest.322d0b5155faf754468b363f82766a89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0056cd46576a2c3740bbf34acbe2c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b0675e4ee76be6c4f1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abf92681ce4874df1c81",
+    "url": "/static/js/2.dffa76fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dffa76fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b0675e4ee76be6c4f1",
+    "url": "/static/js/main.f318fe33.chunk.js"
+  },
+  {
+    "revision": "c56717877b8f0c4cce5b",
+    "url": "/static/js/runtime-main.6babccff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
